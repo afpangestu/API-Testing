810f4a09b0c19538ab8cdc823481bb065ddccc67
Aji Fauzi P <ajifauzi949@gmail.com>
2024-12-18 09:35:20 +0700

    add fucntion user login testing

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def testSum():
-    x = 10
-    y = 20
-    return x + y
+import requests
 
-assert testSum()
+def test_login_user():
+    url = "https://secondhand.binaracademy.org/users/sign_in.json"
+    data = {
+        "user": {
+            "email": "ajiseller@mail.com",
+            "password": "ajiseller"
+        }
+    }
+
+    response = requests.post(url, json=data)
+
+    # Verify status code
+    assert response.status_code == 200
+
+    # Verify content-type
+    assert response.headers["Content-Type"] == "application/json; charset=utf-8"
