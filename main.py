21670400e8048c0f37e60f22c2814d57ff922522
Aji Fauzi P <ajifauzi949@gmail.com>
2024-12-18 09:55:40 +0700

    add URL variable to global

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import requests
 
 url = "https://secondhand.binaracademy.org"
+USER_ID = 0
 def test_login_user():
-    data = {
+    jsonBody = {
         "user": {
             "email": "ajiseller@mail.com",
             "password": "ajiseller"
         }
     }
 
-    response = requests.post(url + "/users/sign_in.json", json=data)
+    response = requests.post(url + "/users/sign_in.json", json=jsonBody)
 
     # Verify status code
     assert response.status_code == 200
 
+    data = response.json()
+
     # Verify content-type
     assert response.headers["Content-Type"] == "application/json; charset=utf-8"
+    print(data)
+
+    # Store response value to variable
+    USER_ID = data['user']['id']
+    print(USER_ID)
+
+def test_get_offer_by_userid():
+    response = requests.get(url + f"/users/{USER_ID}/offers.json")
+
+    # Verify status code (BAKAL ERROR SOALNYA SESI LOGIN GAK KE SIMPEN
+    assert response.status_code == 401
+
+    # Verify content-type
+    assert response.headers["Content-Type"] == "application/json; charset=utf-8"
+
+    data = response.json()
+    print(data)
